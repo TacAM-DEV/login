2b7467c3e72c0233638a79c902f43988d22ab53a
Lukas <github@muffl0n.de>
2020-03-21 21:20:10 +0100

    Add example Environment File

---
+++ src/js/example.env.js
@@ -0,0 +1,12 @@
+const conf = {
+  "SiteName": "TacAM-Login",
+
+  "signup": {
+    "PwRegex": "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  },
+  //Backend-Server for Login/Signup Handling
+  "backend": {
+    "login" : "https://domain.tld/login",
+    "signup": "https://domain.tld/signup"
+  }
+}
