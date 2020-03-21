eb37b3303f085f5431351fbc1b01d1a2537db1d3
Lukas <github@muffl0n.de>
2020-03-21 21:22:01 +0100

    Angular add http

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,30 @@
 var loginApp = angular.module('loginApp', []);
 
 // Define the `loginController` controller on the `loginController` module
-loginApp.controller('loginController', function loginController($scope) {
+loginApp.controller('loginController', ['$scope', '$http', function loginController($scope, $http) {
 
   $scope.SiteName = conf.SiteName;
 
   $scope.signup = function(){
     console.log("SignUn");
-    let Regex = new RegExp(config.signup.PwRegex);
+    let pwregex = new RegExp(conf.signup.PwRegex);
 
-    if(strongRegex.test(password)){
-      //Signup Process
+    if(pwregex.test($scope.password)){
+      $http({
+        method: 'POST',
+        url: conf.backend.signup,
+        data: {
+          name: $scope.name,
+          email: $scope.Email,
+          password: $scope.password
+        }
+      }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
     }else{
       //Notify bad Password
     }
@@ -24,4 +38,4 @@ loginApp.controller('loginController', function loginController($scope) {
   $scope.forgotPassword = function(){
     console.log("Forgot Password");
   }
-});
+}]);
