387abe58f09172589eb33f41d6144925d223abfe
Lukas <github@muffl0n.de>
2020-03-21 18:12:04 +0100

    update conf | add Regex

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,18 @@ var loginApp = angular.module('loginApp', []);
 
 // Define the `loginController` controller on the `loginController` module
 loginApp.controller('loginController', function loginController($scope) {
-  $scope.config = import("./env.js");
 
-  $scope.SiteName = "TacAM-Login";
+  $scope.SiteName = conf.SiteName;
 
   $scope.signup = function(){
-    console.log("SignUp");
+    console.log("SignUn");
+    let Regex = new RegExp(config.signup.PwRegex);
+
+    if(strongRegex.test(password)){
+      //Signup Process
+    }else{
+      //Notify bad Password
+    }
   }
 
   $scope.signin = function(){
@@ -16,6 +22,6 @@ loginApp.controller('loginController', function loginController($scope) {
   }
 
   $scope.forgotPassword = function(){
-    console.log("Forgot Password")
+    console.log("Forgot Password");
   }
 });
