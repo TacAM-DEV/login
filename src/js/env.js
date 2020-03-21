03977c06c10904cf9549e7ea44fe7a32510769b5
Lukas <github@muffl0n.de>
2020-03-21 18:07:19 +0100

    Add Backend-Domain and PWRegex

--- src/js/env.js
+++ src/js/env.js
@@ -1,4 +1,12 @@
-name: {
-    SiteName: "TacAM-Login"
-    Zeug: "Und so"
+const conf = {
+  "SiteName": "TacAM-Login",
+
+  "signup": {
+    "PwRegex": "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  },
+  //Backend-Server for Login/Signup Handling
+  "backend": {
+    "login" : "https://domain.tld/login",
+    "signup": "https://domain.tld/signup"
   }
+}
